960fb5b88bdc29b3c3080888e244db63afa68109
VarMoon <49207914+VarM00n@users.noreply.github.com>
2021-04-03 00:45:22 +0200

    v0.1

---
+++ Activation_Functions.py
@@ -0,0 +1,16 @@
+
+
+def relu(x):
+    return 0 if x < 0 else x
+
+
+def relu_deriv():
+    pass
+
+
+def softmax():
+    pass
+
+
+def softmax_deriv():
+    pass
---
+++ Layer.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+from Activation_Functions import *
+
+# TODO complete activation functions
+# TODO create Neural Network class
+
+class Layer:
+
+    def __init__(self, inputs, neurons, last_layer):
+        self.weights = np.random.uniform(low=-0.5, high=0.5, size=(neurons, inputs))
+        self.bias = np.ones(neurons)
+        self.z = np.zeros(neurons)
+        self.output = np.zeros(neurons)
+        self.last_layer = last_layer
+
+    def forward_propagation(self, input_values):
+        self.z = self.weights.dot(input_values) + self.bias
+        if self.last_layer:
+            self.output = np.array([softmax() for x in self.z])  # TODO fill softmax func
+        else:
+            self.output = np.array([relu(x) for x in self.z])
+        return self.output
+
+
+layer1 = Layer(3, 2, False)
+layer2 = Layer(2, 1, True)
+
+layer1.forward_propagation([0.1, 0.3, 0.6])
+layer2.forward_propagation(layer1.output)
+
+print(layer2.output)
+
+
