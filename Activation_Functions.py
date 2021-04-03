aae30367e97e25cf53e611f2e80b7396d69ec081
VarMoon <49207914+VarM00n@users.noreply.github.com>
2021-04-03 12:55:26 +0200

    v0.2 filled activation functions

--- Activation_Functions.py
+++ Activation_Functions.py
@@ -1,16 +1,15 @@
+import numpy as np
+import math
 
 
 def relu(x):
     return 0 if x < 0 else x
 
 
-def relu_deriv():
-    pass
+def relu_deriv(x):
+    return 0 if x < 0 else 1
 
 
-def softmax():
-    pass
-
-
-def softmax_deriv():
-    pass
+def softmax(array, x):
+    sum_of_exp = np.sum(np.exp(array))
+    return math.exp(x)/sum_of_exp
--- Layer.py
+++ Layer.py
@@ -5,6 +5,7 @@ from Activation_Functions import *
 # TODO complete activation functions
 # TODO create Neural Network class
 
+
 class Layer:
 
     def __init__(self, inputs, neurons, last_layer):
@@ -17,18 +18,22 @@ class Layer:
     def forward_propagation(self, input_values):
         self.z = self.weights.dot(input_values) + self.bias
         if self.last_layer:
-            self.output = np.array([softmax() for x in self.z])  # TODO fill softmax func
+            self.output = np.array([softmax(self.z, x) for x in self.z])
         else:
             self.output = np.array([relu(x) for x in self.z])
         return self.output
 
 
 layer1 = Layer(3, 2, False)
-layer2 = Layer(2, 1, True)
+layer2 = Layer(2, 2, True)
 
 layer1.forward_propagation([0.1, 0.3, 0.6])
 layer2.forward_propagation(layer1.output)
 
+print(layer1.output)
+
 print(layer2.output)
+
+
 
 
