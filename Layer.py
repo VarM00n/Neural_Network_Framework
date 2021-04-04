7e82e3d08e629b848bffbf4d4d1569470038e9cd
VarMoon <49207914+VarM00n@users.noreply.github.com>
2021-04-04 14:05:18 +0200

    v0.4 backpropagation filled

--- Layer.py
+++ Layer.py
@@ -8,10 +8,15 @@ from Activation_Functions import *
 
 class Layer:
 
+    dz = []
+    dw = []
+    db = []
+
     def __init__(self, inputs, neurons, last_layer):
         self.weights = np.random.uniform(low=-0.5, high=0.5, size=(neurons, inputs))
         self.bias = np.ones(neurons)
         self.z = np.zeros(neurons)
+        self.dz = np.zeros(neurons)
         self.output = np.zeros(neurons)
         self.last_layer = last_layer
 
@@ -23,6 +28,20 @@ class Layer:
             self.output = np.array([relu(x) for x in self.z])
         return self.output
 
+    # TODO change updating dw with training_input (not always the same)
+    def back_propagation(self, training_output, training_input, prev_layer=None, next_layer=None):
+        if self.last_layer:
+            self.dz = self.output - training_output
+            temp = self.dz.reshape((-1, 1))
+            self.dw = temp * prev_layer.output
+            self.db = self.dz
+        else:
+            temp1 = next_layer.weights.T
+            temp2 = next_layer.dz.reshape((-1, 1))
+            self.dz = temp1.dot(temp2).T * np.array([relu_deriv(x) for x in self.z])
+            self.dw = self.dz.reshape((-1, 1)) * training_input
+            self.db = self.dz
 
-
-
+    def update_values(self, learning_rate):
+        self.weights = self.weights - learning_rate * self.dw
+        self.bias = self.bias - learning_rate * self.db
--- NN_framework.py
+++ NN_framework.py
@@ -1,13 +1,16 @@
 from Layer import Layer
+import numpy as np
+import csv
 
 
 class NN:
     layers = []
     training_inputs = [[0.3, 0.1, 0.4]]
     training_outputs = [[1, 0]]
+    learning_rate = 0
 
-    def __init__(self):
-        pass
+    def __init__(self, learning_rate):
+        self.learning_rate = learning_rate
 
     def add_layer(self, inputs, neurons, last_layer):
         self.layers.append(Layer(inputs, neurons, last_layer))
@@ -18,11 +21,29 @@ class NN:
             input_for_next_layer = self.layers[i].forward_propagation(input_for_next_layer)
         return input_for_next_layer
 
+    def full_backward_pass(self, num_of_training):
+        for i in range(len(self.layers) - 1, -1, -1):
+            if i - 1 >= 0:
+                prev_layer = nn.layers[i - 1]
+            else:
+                prev_layer = None
+            if i + 1 < len(self.layers):
+                next_layer = nn.layers[i+1]
+            else:
+                next_layer = None
+            nn.layers[i].back_propagation(nn.training_outputs[num_of_training], nn.training_inputs[num_of_training],
+                                          prev_layer, next_layer)
 
-nn = NN()
+    def update_all(self):
+        for i in range(len(self.layers)):
+            self.layers[i].update_values(self.learning_rate)
 
 
-nn.add_layer(3, 2, False)
-nn.add_layer(2, 2, True)
-print(nn.full_forward_pass(0))
-print("ff")
+nn = NN(0.2)
+
+nn.add_layer(784, 10, False)
+nn.add_layer(10, 10, True)
+nn.full_forward_pass(0)
+nn.full_backward_pass(0)
+nn.update_all()
+print("done")
